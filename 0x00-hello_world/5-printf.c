1f82e1e9fec74a1cc1f4792ed1ede2e466094cfd
Abba_KTG <abbaktg876@gmail.com>
2023-02-16 17:44:53 -0800

    added some scripts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,10 @@
 #include <stdio.h>
-int main(void){printf("with proper grammar, but the outcome is a piece of art,\n");return (0);}
+/**
+ *  * Description: main - Prints out a string to stdout.
+ *   * Return: 0 if success.
+ *    */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
