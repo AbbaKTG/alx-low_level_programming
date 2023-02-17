af1c9a58b4728e506dc94b0a13184c208ef01c4f
Abba_KTG <abbaktg876@gmail.com>
2023-02-16 18:46:36 -0800

    made some script edition

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-/*
- *   Description: main - Prints out a string to stdout.
- *   Return: 0 if success.
+/**
+ * *Description: main - Prints out a string to stdout.
+ *
+ *  * Return: 0 if success.
  */
 int main(void)
 {
