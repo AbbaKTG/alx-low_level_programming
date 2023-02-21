3ef56baae02088cbb17516f43e901f5c4e6ca503
Abba_KTG <abbaktg876@gmail.com>
2023-02-21 01:28:48 -0800

    added a script

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * Description - prints _putchar
+ * Return: Always 0, if execution is successful.
+ *
+ */
+int main(void)
+{
+	putschar('_putchar' + 0 + '\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This contains task on "0x02. C - Functions, nested loops"
