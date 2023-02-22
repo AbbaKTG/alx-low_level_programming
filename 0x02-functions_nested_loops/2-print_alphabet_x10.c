e501c4bc55f0013c02927dcd95822fec597423b2
Abba_KTG <abbaktg876@gmail.com>
2023-02-22 15:59:57 -0800

    edited and added some scripts

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_10x - prints the alphabets in lower case x 10
+ * print_alphabet_x10 - prints the alphabets in lower case x 10
  *
  */
 
--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
-/*
+/**
  * _islower - Checks for lowering character
- * c: The character to be checked
+ * @c: The character to be checked
  * Return: 1 for lowercase character or 0 for anything else
  *
  */
