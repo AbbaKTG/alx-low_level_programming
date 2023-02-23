267d76f7ab050e4f1083b122fbd8f53a697e4e98
Abba_KTG <abbaktg876@gmail.com>
2023-02-23 09:09:39 -0800

    edited a program

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,13 +13,11 @@ int main(void)
 
 	i = 0;
 
-/*
- * while (i < 1)
- *	{
- */		putchar(i);
- /*
- *	}
- */
+/*	while (i < 1)*/
+ /*	{*/
+ /*		putchar(i);*/
+ /*	}*/
+
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
