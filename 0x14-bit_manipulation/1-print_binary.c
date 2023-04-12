f3c3a5b430ce3860d84093136b13cdea20192041
AbbaKTG <abbaktg876@gmail.com>
2023-04-11 19:05:46 -0700

    added a task

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -16,13 +16,13 @@ void print_binary(unsigned long int n)
 		return;
 	}
 
-	for (temp = n, shifts = 0; (temp >>= 1) > 0; shifts++);
-
-	for (; shifts >= 0; shifts--)
-	{
-		if ((n >> shifts) & 1)
-			printf("1");
-		else
-			printf("0");
-	}
-}
+	for (temp = n, shifts = 0; (temp >>= 1) > 0; shifts++){
+		for (; shifts >= 0; shifts--)
+		{
+			if ((n >> shifts) & 1){
+				printf("1");
+			}else{
+				printf("0");
+			}	
+		}
+	}	
