6aec88b2c11c7f5dc967ec3beec0129612e009ac
AbbaKTG <abbaktg876@gmail.com>
2023-04-11 18:37:28 -0700

    added task 3

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,5 +2,6 @@
 #define HOLBERTON_H
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
-void print_binary(unsigned long int n);		
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
 #endif
